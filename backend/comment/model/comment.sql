d724a2098e570646604ed6c0ec8bcdf2969dd973
huangsihao7 <1057434651@qq.com>
2023-10-25 22:14:42 +0800

    Feat: constants

--- backend/comment/model/comment.sql
+++ backend/comment/model/comment.sql
@@ -1,3 +1,4 @@
+/*
  Navicat Premium Data Transfer
 
  Source Server         : 2080
@@ -10,24 +11,27 @@
  Target Server Version : 80027 (8.0.27)
  File Encoding         : 65001
 
- Date: 25/10/2023 19:38:08
+ Date: 25/10/2023 20:18:56
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for favorites
+-- Table structure for comments
 -- ----------------------------
-DROP TABLE IF EXISTS `favorites`;
-CREATE TABLE `favorites`  (
-  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-  `uid` int UNSIGNED NOT NULL,
-  `vid` int NOT NULL,
-  `created_at` datetime NULL DEFAULT CURRENT_TIMESTAMP,
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `uid` int UNSIGNED NOT NULL COMMENT '用户id',
+  `vid` int UNSIGNED NOT NULL COMMENT '视频Id',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '评论内容',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NULL DEFAULT NULL,
   `deleted_at` datetime NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `uid`(`uid` ASC) USING BTREE,
+  INDEX `vid`(`vid` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS = 1;
