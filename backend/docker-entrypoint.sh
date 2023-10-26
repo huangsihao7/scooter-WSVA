c16230778e777616fa69c83e15398217a9927741
huangsihao7 <1057434651@qq.com>
2023-10-27 00:09:32 +0800

    Feat: etcd addr and dockerfile

---
+++ backend/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+cd user/rpc && go run user.go -f etc/user.yaml >> output.log 2>&1 &
+cd feed/rpc && go run feed.go -f etc/feed.yaml >> output.log 2>&1 &
+cd favorite/rpc && go run favorite.go -f etc/favorite.yaml >> output.log 2>&1 &
+cd relation/rpc && go run relation.go -f etc/relation.yaml >> output.log 2>&1 &
+cd comment/rpc && go run comment.go -f etc/comment.yaml >> output.log 2>&1 &
+
+
+cd user/api && go run user.go -f etc/user.yaml >> output.log 2>&1 &
+cd feed/api && go run feed.go -f etc/feed.yaml >> output.log 2>&1 &
+cd favorite/api && go run favorite.go -f etc/favorite.yaml >> output.log 2>&1 &
+cd relation/api && go run relation.go -f etc/relation.yaml >> output.log 2>&1 &
+cd comment/api && go run comment.go -f etc/comment.yaml >> output.log 2>&1 &
+
+tail -f output.log
