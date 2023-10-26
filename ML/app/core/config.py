8a77b0ae1d813f8754baf44c1e3fefe10ae7c882
huangsihao7 <1057434651@qq.com>
2023-10-26 11:45:16 +0800

    fix: specified version

--- ML/app/core/config.py
+++ ML/app/core/config.py
@@ -1,18 +1,18 @@
 import tempfile
 
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     WHISPER_MODEL_PATH: str = "openai/whisper-medium"
-    WHISPER_PROMPT = """
+    WHISPER_PROMPT: str = """
 - 我们的口号是什么？
 - 骑上我心爱的小摩托，他永远不会堵车。
 - 我们的目标是？
 - 实习证明！
 """
-    CACHE_ROOT_DIR = tempfile.mkdtemp()
-    DEVICE = "cuda"
+    CACHE_ROOT_DIR: str = tempfile.mkdtemp()
+    DEVICE: str = "cuda"
 
     class Config:
         case_sensitive = True
