9429d6429d0dea3ec63b6acc28bb2eae1b04982e
huangsihao7 <1057434651@qq.com>
2023-10-27 11:16:59 +0800

    feat: sh

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -5,11 +5,10 @@ go run favorite/rpc/favorite.go -f favorite/rpc/etc/favorite.yaml >> /build/outp
 go run relation/rpc/relation.go -f relation/rpc/etc/relation.yaml >> /build/output.log 2>&1 &
 go run comment/rpc/comment.go -f comment/rpc/etc/comment.yaml >> /build/output.log 2>&1 &
 
-
-cd user/api && go run user.go -f etc/user.yaml >> /build/output.log 2>&1 &
-cd feed/api && go run feed.go -f etc/feed.yaml >> /build/output.log 2>&1 &
-cd favorite/api && go run favorite.go -f etc/favorite.yaml >> /build/output.log 2>&1 &
-cd relation/api && go run relation.go -f etc/relation.yaml >> /build/output.log 2>&1 &
-cd comment/api && go run comment.go -f etc/comment.yaml >> /build/output.log 2>&1 &
+go run user/api/user.go -f user/api/etc/user.yaml >> /build/output.log 2>&1 &
+go run feed/api/feed.go -f feed/api/etc/feed.yaml >> /build/output.log 2>&1 &
+go run favorite/api/favorite.go -f favorite/api/etc/favorite.yaml >> /build/output.log 2>&1 &
+go run relation/api/relation.go -f relation/api/etc/relation.yaml >> /build/output.log 2>&1 &
+go run comment/api/comment.go -f comment/api/etc/comment.yaml >> /build/output.log 2>&1 &
 
 tail -f /build/output.log
