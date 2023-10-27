98918bf2ba0cc65d9ed14eecff71bd4e13ffa883
huangsihao7 <1057434651@qq.com>
2023-10-27 11:09:45 +0800

    feat: sh

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,9 +1,9 @@
 echo "start" >> /build/output.log
-cd user/rpc && go run user.go -f etc/user.yaml >> /build/output.log 2>&1 &
-cd feed/rpc && go run feed.go -f etc/feed.yaml >> /build/output.log 2>&1 &
-cd favorite/rpc && go run favorite.go -f etc/favorite.yaml >> /build/output.log 2>&1 &
-cd relation/rpc && go run relation.go -f etc/relation.yaml >> /build/output.log 2>&1 &
-cd comment/rpc && go run comment.go -f etc/comment.yaml >> /build/output.log 2>&1 &
+go run user/rpc/user.go -f user/rpc/etc/user.yaml >> /build/output.log 2>&1 &
+go run feed/rpc/feed.go -f feed/rpc/etc/feed.yaml >> /build/output.log 2>&1 &
+go run favorite/rpc/favorite.go -f favorite/rpc/etc/favorite.yaml >> /build/output.log 2>&1 &
+go run relation/rpc/relation.go -f relation/rpc/etc/relation.yaml >> /build/output.log 2>&1 &
+go run comment/rpc/comment.go -f comment/rpc/etc/comment.yaml >> /build/output.log 2>&1 &
 
 
 cd user/api && go run user.go -f etc/user.yaml >> /build/output.log 2>&1 &
