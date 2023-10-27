3c07c78d54f7e3e9acd23a26cbc75272e3227a69
huangsihao7 <1057434651@qq.com>
2023-10-27 12:09:27 +0800

    feat: comment

--- backend/comment/rpc/internal/svc/servicecontext.go
+++ backend/comment/rpc/internal/svc/servicecontext.go
@@ -4,6 +4,7 @@ import (
 	"github.com/huangsihao7/scooter-WSVA/comment/model"
 	"github.com/huangsihao7/scooter-WSVA/comment/rpc/internal/config"
 	model2 "github.com/huangsihao7/scooter-WSVA/feed/model"
+	model3 "github.com/huangsihao7/scooter-WSVA/user/model"
 	"github.com/huangsihao7/scooter-WSVA/user/rpc/usesrv"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 	"github.com/zeromicro/go-zero/zrpc"
@@ -14,6 +15,7 @@ type ServiceContext struct {
 	Model      model.CommentsModel
 	VideoModel model2.VideosModel
 	UserRpc    usesrv.UseSrv
+	UserModel  model3.UserModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
@@ -22,5 +24,6 @@ func NewServiceContext(c config.Config) *ServiceContext {
 		Model:      model.NewCommentsModel(sqlx.NewMysql(c.DataSource)),
 		VideoModel: model2.NewVideosModel(sqlx.NewMysql(c.DataSource)),
 		UserRpc:    usesrv.NewUseSrv(zrpc.MustNewClient(c.UserRpc)),
+		UserModel:  model3.NewUserModel(sqlx.NewMysql(c.DataSource), c.Cache),
 	}
 }
