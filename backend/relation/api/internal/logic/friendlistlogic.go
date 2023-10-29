89f97a889a5b5c893aaa39e5119a71fc2368ce85
wy <867042655@qq.com>
2023-10-29 19:24:12 +0800

    fixed: 关系里增加背景图

--- backend/relation/api/internal/logic/friendlistlogic.go
+++ backend/relation/api/internal/logic/friendlistlogic.go
@@ -38,12 +38,13 @@ func (l *FriendListLogic) FriendList(req *types.FriendListReq) (resp *types.Frie
 	resList := make([]types.UserInfo, 0)
 	for _, item := range list.List {
 		resList = append(resList, types.UserInfo{
-			Id:     item.Id,
-			Name:   item.Name,
-			Gender: item.Gender,
-			Mobile: item.Mobile,
-			Avatar: item.Avatar,
-			Dec:    item.Dec,
+			Id:      item.Id,
+			Name:    item.Name,
+			Gender:  item.Gender,
+			Mobile:  item.Mobile,
+			Avatar:  item.Avatar,
+			Dec:     item.Dec,
+			BackImg: item.BackImg,
 		})
 	}
 	return &types.FriendListResp{
