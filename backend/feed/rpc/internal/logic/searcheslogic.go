df6e0639a5540a8b9ecd3010eec1f9276f23efd1
huangsihao7 <1057434651@qq.com>
2023-10-31 22:29:46 +0800

    fix: search logics

---
+++ backend/feed/rpc/internal/logic/searcheslogic.go
@@ -0,0 +1,40 @@
+package logic
+
+import (
+	"context"
+	"github.com/huangsihao7/scooter-WSVA/common/constants"
+	"github.com/huangsihao7/scooter-WSVA/feed/rpc/feed"
+	"github.com/huangsihao7/scooter-WSVA/feed/rpc/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type SearchESLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewSearchESLogic(ctx context.Context, svcCtx *svc.ServiceContext) *SearchESLogic {
+	return &SearchESLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *SearchESLogic) SearchES(in *feed.EsSearchReq) (*feed.EsSearchResp, error) {
+	// todo: add your logic here and delete this line
+	//content := in.Content
+	//put2, err := l.svcCtx.Es.Index.
+	//	Index("megacorp").
+	//	Type("employee").
+	//	Id("2").
+	//	BodyJson(e2).
+	//	Do(context.Background())
+
+	return &feed.EsSearchResp{
+		StatusCode: constants.ServiceOKCode,
+		StatusMsg:  constants.ServiceOK,
+	}, nil
+}
