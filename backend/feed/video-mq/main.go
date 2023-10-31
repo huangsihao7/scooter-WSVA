dc4da061f125f1f255a72f473f4d4f73e6986252
huangsihao7 <1057434651@qq.com>
2023-10-31 23:46:50 +0800

    fix: post danmu

--- backend/feed/video-mq/main.go
+++ backend/feed/video-mq/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"github.com/zeromicro/go-zero/core/logx"
 	"log"
 
 	"github.com/huangsihao7/scooter-WSVA/feed/video-mq/internal/config"
@@ -17,7 +18,7 @@ var configFile = flag.String("f", "etc/video-mq-dev.yaml", "the config file")
 
 func main() {
 	flag.Parse()
-
+	logx.DisableStat()
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
 
