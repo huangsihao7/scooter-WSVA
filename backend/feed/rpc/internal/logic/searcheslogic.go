416843a657660aaef1beb4f1713f5759d3a40734
huangsihao7 <1057434651@qq.com>
2023-10-31 22:30:18 +0800

    fix: search logics

--- backend/feed/rpc/internal/logic/searcheslogic.go
+++ backend/feed/rpc/internal/logic/searcheslogic.go
@@ -15,6 +15,7 @@ type SearchESLogic struct {
 	logx.Logger
 }
 
+// 123
 func NewSearchESLogic(ctx context.Context, svcCtx *svc.ServiceContext) *SearchESLogic {
 	return &SearchESLogic{
 		ctx:    ctx,
--- backend/feed/rpc/internal/logic/url_test.go
+++ backend/feed/rpc/internal/logic/url_test.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	es "github.com/elastic/go-elasticsearch/v8"
 	"io"
@@ -9,21 +10,6 @@ import (
 	"testing"
 )
 
-type Hits struct {
-	Total struct {
-		Value    int    `json:"value"`
-		Relation string `json:"relation"`
-	} `json:"total"`
-	MaxScore float64 `json:"max_score"`
-	Hits     []struct {
-		Index  string  `json:"_index"`
-		ID     string  `json:"_id"`
-		Score  float64 `json:"_score"`
-		Source struct {
-			ID int `json:"id"`
-		} `json:"_source"`
-	} `json:"hits"`
-}
 type Response struct {
 	Took     int  `json:"took"`
 	TimedOut bool `json:"timed_out"`
@@ -94,22 +80,21 @@ func TestUrl(t *testing.T) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	//var result map[string]interface{}
-	//err = json.Unmarshal(bodyBytes, &Hits{})
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	res := string(bodyBytes)
-	fmt.Println(res)
-	//var hits Hits
-	//err = json.Unmarshal([]byte(response), &hits)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//assert := assert.New(t)
-	//assert.Equal(2, hits.Total.Value)
-	//for _, hit := range hits.Hits {
-	//	t.Logf("ID: %d", hit.Source.ID)
-	//}
+
+	var responses Response
+	err = json.Unmarshal([]byte(bodyBytes), &responses)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 使用结构体中的字段进行操作
+	took := responses.Took
+	timedOut := responses.TimedOut
+	totalHits := responses.Hits.Total.Value
+
+	log.Println("Took:", took)
+	log.Println("Timed Out:", timedOut)
+	log.Println("Total Hits:", totalHits)
+	log.Println("hit 1:", responses.Hits.Hits[0].ID)
+	log.Println("hit 2:", responses.Hits.Hits[1].ID)
 }
