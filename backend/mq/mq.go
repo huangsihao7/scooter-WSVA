c740c9b3835e88231a736c98c20aac69fba00b53
Mrkuib <67945139+Mrkuib@users.noreply.github.com>
2023-11-07 21:06:26 +0800

    Update mq.go

--- backend/mq/mq.go
+++ backend/mq/mq.go
@@ -20,7 +20,7 @@ func main() {
 	flag.Parse()
 	logx.DisableStat()
 	var c config.Config
-	conf.MustLoad(*configFile, &c)
+	conf.MustLoad(*configFile, &c, conf.UseEnv())
 
 	server := rest.MustNewServer(c.RestConf)
 	defer server.Stop()
