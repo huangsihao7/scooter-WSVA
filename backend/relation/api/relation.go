17d4146a5d5d3d45f1a30e5d90d0cdbd07c4265a
Mrkuib <67945139+Mrkuib@users.noreply.github.com>
2023-11-07 21:05:49 +0800

    Update relation.go

--- backend/relation/api/relation.go
+++ backend/relation/api/relation.go
@@ -21,7 +21,7 @@ var configFile = flag.String("f", "etc/relation.yaml", "the config file")
 func main() {
 	flag.Parse()
 	var c config.Config
-	conf.MustLoad(*configFile, &c)
+	conf.MustLoad(*configFile, &c, conf.UseEnv())
 	server := rest.MustNewServer(c.RestConf, rest.WithCustomCors(nil, func(w http.ResponseWriter) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
